c5b3bad51335086983f32acb7c563b610644b429
George Lobo <77250912+wolff453@users.noreply.github.com>
2022-02-13 19:05:53 -0300

    remove dotenv

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import express from 'express'
 import config from './config/index.js'
 import users from './routes/userRoutes.js'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 const server = express()
 
